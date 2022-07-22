8ca478fa6c8f68fa4eb2a7b476c7952f1d1a680a
Robert Morávek <robert.moravek@gmail.com>
2022-07-22 19:05:54 +0200

    hopefully fixed secrets.json require error

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 const spicedPg = require("spiced-pg");
 const bcrypt = require("bcryptjs");
-const {user, password, database} = require("./secrets.json");
 
-const dbUrl = process.env.DATABASE_URL || `postgres:${user}:${password}@localhost:5432/${database}`;
+let dbUrl;
+if (process.env.NODE_ENV == 'production') {
+    dbUrl = process.env.DATABASE_URL;
+} else {
+    const {user, password, database} = require("./secrets.json");
+    dbUrl = `postgres:${user}:${password}@localhost:5432/${database}`;
+
+}
+
 const db = spicedPg(dbUrl);
 
 module.exports.getNumOfSigners = () => {
